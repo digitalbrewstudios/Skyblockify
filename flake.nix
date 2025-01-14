37c21b64894fe6bac585503eb66a01c12258dd08
eveeifyeve <88671402+Eveeifyeve@users.noreply.github.com>
2025-01-15 04:00:23 +1100

    feat: kotlin project init

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+	inputs = {
+		nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+		flake-parts = { url = "github:hercules-ci/flake-parts"; inputs.nixpkgs.follows = "nixpkgs"; };
+		flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+	};
+
+	outputs = inputs@{ nixpkgs, flake-parts, ... }: 
+		flake-parts.lib.mkFlake {inherit inputs;} {
+			systems = nixpkgs.lib.systems.flakeExposed;
+			perSystem = {
+				lib,
+				pkgs,
+				system,
+				config,
+				...
+			}:
+			let
+				jdk = pkgs.zulu;
+			in
+			{
+				_module.args.pkgs = import nixpkgs {
+					inherit system;
+					config.allowUnsupportedSystem = true;
+				};
+				devShells.default = pkgs.mkShell {
+					nativeBuildInputs = with pkgs; [
+						jdk
+						gradle
+						(kotlin.override {
+							jre = jdk;
+						})
+						(gradle_8.override {
+							java = jdk;
+						})
+					];
+
+					JAVA_HOME = jdk.home;
+				};
+			};
+		};
+}
